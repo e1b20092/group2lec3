53650624c89946279d411c2a5a9455bfbf744fb6
e1b20092 <e1b20092@st.oit.ac.jp>
2022-10-11 14:12:58 +0900

    初期化

---
+++ src/main/java/oit/is/group2/group2lec3/Group2lec3Application.java
@@ -0,0 +1,13 @@
+package oit.is.group2.group2lec3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group2lec3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group2lec3Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/group2/group2lec3/Group2lec3ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.group2.group2lec3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group2lec3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
